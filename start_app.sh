d0e29a65ba46d1e66aeeb68740e14ee0334c0429
Victoria Rozhina <victoriarozhina@gmail.com>
2017-03-28 17:15:07 -0700

    fixed start script location for CF and changed docker run command

--- start_app.sh
+++ start_app.sh
@@ -1,1 +1,1 @@
-sudo docker run --rm --name hello_world_django -it -p 8000:8000 -v /webapp:/hello_world_django quay.io/sandaar/django_unicorn:v1 /bin/sh -c 'cd /hello_world_django/hello_world_django; ../start.sh' 
+docker run --name hello_world_django -d -p 8000:8000 -v /webapp:/hello_world_django quay.io/sandaar/django_unicorn:v1 /bin/sh -c 'cd /hello_world_django/hello_world_django; ../start.sh' 
