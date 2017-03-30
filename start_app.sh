4c9e182247025b25c532729de88d832d11791761
Victoria Rozhina <victoriarozhina@gmail.com>
2017-03-30 09:37:24 -0700

    added nginx

--- start_app.sh
+++ start_app.sh
@@ -1,1 +1,3 @@
 docker run --name hello_world_django -d -p 8000:8000 -v /webapp:/hello_world_django quay.io/sandaar/django_unicorn:v1 /bin/sh -c 'cd /hello_world_django/hello_world_django; ../start.sh' 
+
+docker run -d --name app-nginx -p 80:80 --link hello_world_django:backend -v /webapp/nginx/:/etc/nginx/conf.d/ nginx 
--- stop_app.sh
+++ stop_app.sh
@@ -3,5 +3,12 @@ if [[ $(docker ps -a --filter="name=hello_world_django" | wc -l) -gt 1 ]]; then
     docker stop $CONTAINER_ID
     docker rm $CONTAINER_ID
 else
-    echo "good to go, container not present"
+    echo "good to go, gunicorn/django container not present"
+fi
+if [[ $(docker ps -a --filter="name=app-nginx" | wc -l) -gt 1 ]]; then
+    CONTAINER_ID=$(docker ps -a --filter="name=app-nginx" | tail -n1 | awk '{ print $1 }')
+    docker stop $CONTAINER_ID
+    docker rm $CONTAINER_ID
+else
+    echo "good to go, nginx container not present"
 fi
