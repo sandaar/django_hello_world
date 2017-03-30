3c0776e95d52b1a426c75d9b7ac0374f1db18d04
Victoria Rozhina <victoriarozhina@gmail.com>
2017-03-30 00:23:53 -0700

    fixed container name

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,9 @@ CF_TEMPLATE_BODY="file://$(pwd)/cf.json"
 SERVICE_ROLE_ARN="arn:aws:iam::767256184518:role/CodeDeployServiceRole"
 DEPLOYMENT_DESCRIPTION="staging deployment"
 GITHUB_REPO="sandaar/django_hello_world"
-COMMIT_ID="bba6df8da50bada7a336116a57aea0e45a81dfb4"
+COMMIT_ID="355b7f8560b0bc56ba79b2ad292a15c29ae2e2aa"
 DEPLOYMENT_TYPE="BLUE_GREEN"
 DEPLOYMENT_OPTION="WITHOUT_TRAFFIC_CONTROL"
 
-aws deploy create-deployment-group --application-name $APPLICATION_NAME --ec2-tag-filters Key=Name,Type=KEY_AND_VALUE,Value=$TAG_VALUE --deployment-group-name $DEPLOYMENT_GROUP_NAME --service-role-arn $SERVICE_ROLE_ARN --deployment-style deploymentType=$DEPLOYMENT_TYPE,deploymentOption=$DEPLOYMENT_OPTION
-aws deploy create-deployment --application-name $APPLICATION_NAME --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name $DEPLOYMENT_GROUP_NAME --description $DEPLOYMENT_DESCRIPTION --github-location repository=$GITHUB_REPO,commitId=$COMMIT_ID
+# aws deploy create-deployment-group --application-name $APPLICATION_NAME --ec2-tag-filters Key=Name,Type=KEY_AND_VALUE,Value=$TAG_VALUE --deployment-group-name $DEPLOYMENT_GROUP_NAME --service-role-arn $SERVICE_ROLE_ARN 
+aws deploy create-deployment --application-name $APPLICATION_NAME --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name $DEPLOYMENT_GROUP_NAME --description "$DEPLOYMENT_DESCRIPTION" --github-location repository=$GITHUB_REPO,commitId=$COMMIT_ID
--- stop_app.sh
+++ stop_app.sh
@@ -1,5 +1,5 @@
-if [[ $(docker ps -a --filter="name=some" | wc -l) -gt 1 ]]; then
-    CONTAINER_ID=$(docker ps -a --filter="name=some" | tail -n1 | awk '{ print $1 }')
+if [[ $(docker ps -a --filter="name=hello_world_django" | wc -l) -gt 1 ]]; then
+    CONTAINER_ID=$(docker ps -a --filter="name=hello_world_django" | tail -n1 | awk '{ print $1 }')
     docker stop $CONTAINER_ID
     docker rm $CONTAINER_ID
 else
