5b4795b745a64f1d6b172e7bd9d955bde9bc5dc5
Victoria <victoriarozhina@gmail.com>
2017-04-06 20:35:14 +0000

    wait for deployment to complete

--- maintenance.sh
+++ maintenance.sh
@@ -8,14 +8,17 @@ S3_LOCATION=general_deploy_bucket
 ROOT=/build
 PAGE_LOCATION=$ROOT/nginx
 MESSAGE=""
+CODE=200
 
 
 if [ $1 = "on" ] && [ -f $PAGE_LOCATION/maintenance_off.html ]; then
   mv $PAGE_LOCATION/maintenance_off.html $PAGE_LOCATION/maintenance_on.html;
   MESSAGE="switching loadbalancer to maintenance mode";
+  CODE=503
 elif [ $1 = "off" ] && [ -f $PAGE_LOCATION/maintenance_on.html ]; then
   mv $PAGE_LOCATION/maintenance_on.html $PAGE_LOCATION/maintenance_off.html;
   MESSAGE="switching OFF maintenance mode";
+  CODE=200
 else
   echo "nothing was changed";
   exit 0;
@@ -25,7 +28,7 @@ aws deploy push --application-name $LB_APPLICATION_NAME --s3-location s3://$S3_L
 aws deploy create-deployment --application-name $LB_APPLICATION_NAME --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name $LB_DEPLOYMENT_GROUP_NAME --description "$DEPLOYMENT_DESCRIPTION" --s3-location bucket=$S3_LOCATION,bundleType=zip,key=LoadbalancerApp.zip
 
 lb_dns_name=$(aws ec2 describe-instances --filter Name=tag:Name,Values=$LB_NAME_TAG --query "Reservations[].Instances[][PublicDnsName]" --output=text)
-while [[ $(curl -s -o /dev/null -w "%{http_code}" $lb_dns_name) != 503 ]]; do
+while [[ $(curl -s -o /dev/null -w "%{http_code}" $lb_dns_name) != $CODE ]]; do
   sleep 2
   echo "$MESSAGE"
 done
--- start_app.sh
+++ start_app.sh
@@ -10,7 +10,17 @@ ROOT=/build
 $ROOT/maintenance.sh on
 
 aws deploy push --application-name $APPLICATION_NAME --s3-location s3://$S3_LOCATION/App.zip --ignore-hidden-files --source $ROOT/app
-aws deploy create-deployment --application-name $APPLICATION_NAME --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name $DEPLOYMENT_GROUP_NAME --description "$DEPLOYMENT_DESCRIPTION" --s3-location bucket=$S3_LOCATION,bundleType=zip,key=App.zip 
+output=$(aws deploy create-deployment --application-name $APPLICATION_NAME --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name $DEPLOYMENT_GROUP_NAME --description "$DEPLOYMENT_DESCRIPTION" --s3-location bucket=$S3_LOCATION,bundleType=zip,key=App.zip)
+deployment_id=$(echo "$output" | sed '2q;d' | awk -v FS="(\"deploymentId\": \"|\")" '{print $2}')
+
+# wait for deployment completion
+cmd="aws deploy get-deployment --deployment-id $deployment_id --query "deploymentInfo.status" --output text"
+status=$(eval $cmd)
+while ! [[ $status =~ ^(Failed|Succeeded|Stopped|Skipped)$ ]]; do
+    sleep 5
+    echo "deployment status $status"
+    status=$(eval $cmd)
+done
+echo "deployment completed with status $status"
 
 $ROOT/maintenance.sh off
-
