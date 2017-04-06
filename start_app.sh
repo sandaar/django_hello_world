0cd22b594e5d330cdab854e598a5c8c363503903
root <root@ip-172-31-28-80.ec2.internal>
2017-04-06 17:14:54 +0000

    uncomment maintenance code in start script

--- start_app.sh
+++ start_app.sh
@@ -7,10 +7,10 @@ DEPLOYMENT_GROUP_NAME="Staging"
 ROOT=/build
 
 
-#$ROOT/maintenance.sh on
+$ROOT/maintenance.sh on
 
 aws deploy push --application-name $APPLICATION_NAME --s3-location s3://$S3_LOCATION/App.zip --ignore-hidden-files --source $ROOT/app
 aws deploy create-deployment --application-name $APPLICATION_NAME --deployment-config-name CodeDeployDefault.OneAtATime --deployment-group-name $DEPLOYMENT_GROUP_NAME --description "$DEPLOYMENT_DESCRIPTION" --s3-location bucket=$S3_LOCATION,bundleType=zip,key=App.zip 
 
-#$ROOT/maintenance.sh off
+$ROOT/maintenance.sh off
 
