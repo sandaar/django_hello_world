a4accd00bc41c6dce794472e097610d092571cb9
Victoria Rozhina <victoriarozhina@gmail.com>
2017-04-10 14:44:08 -0700

    copy deploy code recursively if nginx container is not present

--- nginx/start_app.sh
+++ nginx/start_app.sh
@@ -3,11 +3,12 @@ if [ -f /lb_app ]; then
 else
   rm -f /lb_app/*
 fi
-cp /nginx/* /lb_app
 
 if [[ $(docker ps -a --filter="name=app-nginx" | wc -l) -gt 1 ]]; then
+    cp /nginx/* /lb_app
     echo "app-nginx container already present" 
 else
+    cp -r /nginx/* /lb_app
     docker run -d --name app-nginx -p 80:80 -v /lb_app/conf.d/:/etc/nginx/conf.d/ -v /lb_app:/lb_app nginx 
 fi
 
